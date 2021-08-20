7d74dbf6dd557bafabad830e49d275e9bc1985cb
Ian Louw <ianlouw25@gmail.com>
2021-08-20 16:53:04 +0200

    test commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //Selectors
+//Comment
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
